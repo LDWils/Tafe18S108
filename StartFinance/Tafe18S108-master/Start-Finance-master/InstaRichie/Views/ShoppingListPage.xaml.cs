b2587622920c776e46718da68ee798e0eeee5161
Leon Wilson <leon.wilson@student.tafesa.edu.au>
2018-04-06 14:15:15 +0930

    Committing changes made during session 6/04/2018.
    Added ShoppingList.cs
    Added ShoppingListPage.xaml <Not Complete>
    
    Committed by Leon Wilson

---
+++ StartFinance/Tafe18S108-master/Start-Finance-master/InstaRichie/Views/ShoppingListPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿//Shopping List Page View for Start Finance
+//Date last modified 06/04/2018
+//@Author: Leon Wilson
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using SQLite;
+using StartFinance.Models;
+using Windows.UI.Popups;
+using SQLite.Net;
+
+namespace StartFinance.Views
+{
+    public sealed partial class ShoppingList : Page
+    {
+        SQLiteConnection conn; // adding an SQLite connection
+        string path = Path.Combine(Windows.Storage.ApplicationData.Current.LocalFolder.Path, "Findata.sqlite");
+
+        public ShoppingList()
+        {
+            this.InitializeComponent();
+            NavigationCacheMode = Windows.UI.Xaml.Navigation.NavigationCacheMode.Enabled;
+            /// Initializing a database
+            conn = new SQLite.Net.SQLiteConnection(new SQLite.Net.Platform.WinRT.SQLitePlatformWinRT(), path);
+            // Creating table
+            Results();
+        }
+
+        public void Results()
+        {
+            conn.CreateTable<ShoppingList>();
+            var query1 = conn.Table<ShoppingList>();
+            ShopListView.ItemsSource = query1.ToList();
+        }
+    }
+}
