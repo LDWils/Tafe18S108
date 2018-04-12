c6c6a32b7610170a7173a615230401f92c65ee7e
SeChang <electrixocketaus@gmail.com>
2018-04-13 07:38:06 +0930

    Add, Edit, Delete function has finished

--- Start-Finance-master/InstaRichie/Views/AppointmentPage.xaml.cs
+++ Start-Finance-master/InstaRichie/Views/AppointmentPage.xaml.cs
@@ -39,16 +39,137 @@ namespace StartFinance.Views
             // Creating table
             Results();
         }
+
+
         public void Results()
         {
             conn.CreateTable<Appointment>();
-            var query1 = conn.Table<Appointment>();
-            //WishListView.ItemsSource = query1.ToList();
-        }
-        private void Page_Loaded(object sender, RoutedEventArgs e)
-        {
-            ///Results();
+            var query = conn.Table<Appointment>();
+            AppoingmentListView.ItemsSource = query.ToList();
         }
 
+
+        private async void AddAppointment_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string CDay = DPEDate.Date.Day.ToString();
+                string CMonth = DPEDate.Date.Month.ToString();
+                string CYear = DPEDate.Date.Year.ToString();
+                string FinalDate = "" + CMonth + "/" + CDay + "/" + CYear;
+
+
+                string CTime = TPTime.Time.ToString();
+
+                if (TBEName.Text.ToString() == "")
+                {
+                    MessageDialog dialog = new MessageDialog("No value entered", "Event Name");
+                    await dialog.ShowAsync();
+                }
+                else if (TBLocation.Text.ToString() == "")
+                {
+                    MessageDialog dialog = new MessageDialog("No value entered", "Location");
+                    await dialog.ShowAsync(); ;
+                }
+                else if (DPEDate == null)
+                {
+                    MessageDialog dialog = new MessageDialog("No value entered", "Choose The Date");
+                    await dialog.ShowAsync();
+                }
+                else
+                {
+                    conn.Insert(new Appointment()
+                    {
+                        EventName = TBEName.Text,
+                        Location = TBLocation.Text,
+                        EventDate = FinalDate,
+                        EndTime = CTime
+                    });
+                    Results();
+                }
+            }
+            catch (Exception ex)
+            {
+                if (ex is FormatException)
+                {
+                    MessageDialog dialog = new MessageDialog("You forgot to enter the Amount or entered an invalid Amount", "Oops..!");
+                    await dialog.ShowAsync();
+                }
+                else if (ex is SQLiteException)
+                {
+                    MessageDialog dialog = new MessageDialog("Wish Name already exist, Try Different Name", "Oops..!");
+                    await dialog.ShowAsync();
+                }
+            }
+        }
+
+        private async void EditAppointment_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                int AccSelection = ((Appointment)AppoingmentListView.SelectedItem).AppointmentID;
+                if (AccSelection == 0)
+                {
+                    MessageDialog dialog = new MessageDialog("Not selected the Item", "Oops..!");
+                    await dialog.ShowAsync();
+                }
+                else
+                {
+                    conn.CreateTable<Appointment>();
+                    var query1 = conn.Table<Appointment>();
+                    string _eventName = TBEName.Text;
+                    string _Location = TBLocation.Text;
+                    string CDay = DPEDate.Date.Day.ToString();
+                    string CMonth = DPEDate.Date.Month.ToString();
+                    string CYear = DPEDate.Date.Year.ToString();
+                    string FinalDate = "" + CMonth + "/" + CDay + "/" + CYear;
+                    string CTime = TPTime.Time.ToString();
+                    var query3 = conn.Query<Appointment>("UPDATE Appointment SET EventName ='" + TBEName.Text + "'" + ", Location ='" +
+                                                                    TBLocation.Text + "'" + ", EventDate ='" +
+                                                                    FinalDate + "'" + ", EndTime ='" +
+                                                                    CTime + "'" + " WHERE AppointmentID ='" +
+                                                                    AccSelection + "'");
+                    AppoingmentListView.ItemsSource = query1.ToList();
+                }
+            }
+            catch (NullReferenceException)
+            {
+                MessageDialog dialog = new MessageDialog("Not selected the Item", "Oops..!");
+                await dialog.ShowAsync();
+            }
+        }
+
+        private async void DeleteAppointment_Click(object sender, RoutedEventArgs e)
+        {
+            //  var query3 = conn.Query<Appointment>("DROP TABLE Appointment");
+            try
+            {
+                string AccSelection = ((Appointment)AppoingmentListView.SelectedItem).EventName;
+
+                if (AccSelection == "")
+                {
+                    MessageDialog dialog = new MessageDialog("Not selected the Item", "Oops..!");
+                    await dialog.ShowAsync();
+                }
+                else
+                {
+                    conn.CreateTable<Appointment>();
+                    var query1 = conn.Table<Appointment>();
+                    var query3 = conn.Query<Appointment>("DELETE FROM Appointment WHERE EventName ='" + AccSelection + "'");
+                    AppoingmentListView.ItemsSource = query1.ToList();
+                }
+              
+            }
+            catch (NullReferenceException)
+            {
+                MessageDialog dialog = new MessageDialog("Not selected the Item", "Oops..!");
+                await dialog.ShowAsync();
+            }
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            Results();
+        }
     }
 }
